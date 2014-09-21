d34e94ecbb5265251dc24f13de5e0168944cf889
oakoak <dubizhanskii@mail.ru>
2014-09-21 23:19:46 +0400

    Update quadratic.cpp
    No magic constant. No else branch. check_values now displays an error message in the corresponding root

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,79 +1,82 @@
 #include <stdio.h>
 #include <math.h>
 
+#define no_roots 0
+#define one_root 1
+#define two_roots 2
+#define infinitely_roots -1
 
 int quadratic( double a, double b, double c, char* n, double* x1, double* x2)
 {
+    if ((a == 0) && (b == 0) && (c == 0))
+    {
+        *n = infinitely_roots;
+        return 0;
+    }
+
+    if ((a == 0) && (b == 0))
+    {
+        *n = no_roots;
+        return 0;
+    }
+
     if (a == 0)
     {
-        if (b == 0)
-        {
-            if (c == 0)
-            {
-                *n = 3;
-            }
-            else
-            {
-                *n = 0;
-            }
-        }
-        else
-        {
-            *n = 1;
-            *x1 = -c/b;
-        }
+        *n = one_root;
+        *x1 = -c/b;
+        return 0;
+    }
+
+    double discriminant = 0;
+    discriminant = b*b - 4*a*c;
+    if (discriminant < 0)
+    {
+        *n = no_roots;
     }
     else
     {
-        double discriminant = 0;
-        discriminant = b*b - 4*a*c;
-        if (discriminant < 0)
+        if (discriminant == 0)
         {
-            *n = 0;
+            *n = one_root;
+            *x1 =  -b/(2*a);
         }
         else
         {
-            if (discriminant == 0)
-            {
-                *n = 1;
-                *x1 =  -b/(2*a);
-            }
-            else
-            {
-                *n = 2;
-                discriminant = sqrt(discriminant);
-                *x1 = ( -b - discriminant) / (2*a);
-                *x2 = ( -b + discriminant) / (2*a);
-            }
+            *n = two_roots;
+            discriminant = sqrt(discriminant);
+            *x1 = ( -b - discriminant) / (2*a);
+            *x2 = ( -b + discriminant) / (2*a);
         }
     }
+
     return 0;
 }
 
 int check_values (double a, double b, double c, char n, double x1, double x2)
 {
-    if (((n == 1) || (n == 2)) && ( a*x1*x1 + b*x1 + c != 0 ))
-        return 1;
+    if (((n == one_root) || (n == two_roots)) && ( a*x1*x1 + b*x1 + c != 0 ))
+        printf("root x1 is not correct");
 
-    if ((n == 2) && ( a*x2*x2 + b*x2 + c != 0 ))
-        return 1;
+    if ((n == two_roots) && ( a*x2*x2 + b*x2 + c != 0 ))
+        printf("root x2 is not correct");
     return 0;
 }
 
 
 int print_roots (char n, double x1, double x2)
 {
-    if (n == 0)
-        printf("No radicals\n");
+    if (n == no_roots)
+        printf("No roots\n");
 
-    if (n == 3)
+    if (n == infinitely_roots)
         printf("Infinitely roots\n");
 
-    if (n == 1)
+    if (n == one_root)
         printf("One root: %lg\n", x1);
 
-    if (n == 2)
+    if (n == two_roots)
         printf("Two root: %lg, %lg\n", x1, x2);
+
     return 0;
 }
 
@@ -85,8 +88,7 @@ int main()
     double x1 = 0,x2 = 0;
     char n = 0;
     quadratic(a, b, c, &n, &x1, &x2);
-    if (check_values(a, b, c, n, x1, x2) != 0)
-    printf("Roots do not correspond to reality\n\n");
     print_roots(n, x1, x2);
+    check_values(a, b, c, n, x1, x2);
     return 0;
 }
